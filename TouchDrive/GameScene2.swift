7badfb1eef12ff17f6f2af3c0de3e6eeaac56dad
Hesedel <hes@pajaroncreative.com>
2015-08-02 03:18:20 +0200

    2nd Commit

--- TouchDrive/GameScene2.swift
+++ TouchDrive/GameScene2.swift
@@ -10,6 +10,7 @@ import SpriteKit
 
 class GameScene2: SKScene {
     let road = SKNode()
+    var roads = [SKShapeNode]()
     let myCar = SKShapeNode(rectOfSize:CGSize(width:50, height:100))
     let myControl = SKShapeNode(circleOfRadius:25)
     
@@ -22,8 +23,26 @@ class GameScene2: SKScene {
         
         //self.addChild(myLabel)
         
-        road.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        road.position = CGPoint(x:CGRectGetMidX(self.frame), y:0);
         self.addChild(road)
+        
+        roads.append(SKShapeNode(rectOfSize:CGSize(width:100, height:self.frame.height)))
+        roads[roads.count-1].fillColor = UIColor(red:0, green:0, blue:255, alpha:1);
+        roads[roads.count-1].strokeColor = UIColor(red:0, green:0, blue:255, alpha:1);
+        roads[roads.count-1].position = CGPoint(x:0, y:CGRectGetMidY(self.frame));
+        road.addChild(roads[roads.count-1])
+        
+        myCar.fillColor = UIColor(red:255, green:0, blue:0, alpha:1);
+        myCar.strokeColor = UIColor(red:255, green:0, blue:0, alpha:1);
+        myCar.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+        self.addChild(myCar)
+        
+        myControl.fillColor = UIColor(red:0, green:255, blue:0, alpha:1);
+        myControl.strokeColor = UIColor(red:0, green:255, blue:0, alpha:1);
+        myControl.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame)-100);
+        
+        self.addChild(myControl)
     }
     
     override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
@@ -32,21 +51,45 @@ class GameScene2: SKScene {
         for touch in (touches as! Set<UITouch>) {
             let location = touch.locationInNode(self)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            //let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
+            //sprite.xScale = 0.5
+            //sprite.yScale = 0.5
+            //sprite.position = location
             
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            //let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
             
-            sprite.runAction(SKAction.repeatActionForever(action))
+            //sprite.runAction(SKAction.repeatActionForever(action))
             
-            self.addChild(sprite)
+            //self.addChild(sprite)
         }
     }
-   
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        for touch in (touches as! Set<UITouch>) {
+            let location = touch.locationInNode(self)
+            
+            myControl.position = location
+            myCar.position = CGPoint(x:location.x, y:location.y+100)
+        }
+    }
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
+        
+        road.position.y -= 10
+        
+        loadRoad()
+    }
+    
+    func loadRoad() {
+        if (roads.count - 1 == Int(floor(abs(road.position.y) / self.frame.height))) {
+            println(String(roads.count - 1) + " road loaded")
+            roads.append(SKShapeNode(rectOfSize:CGSize(width:100, height:self.frame.height)))
+            roads[roads.count-1].fillColor = UIColor(red:0, green:255, blue:255, alpha:1);
+            roads[roads.count-1].strokeColor = UIColor(red:0, green:0, blue:255, alpha:1);
+            roads[roads.count-1].position = CGPoint(x:0, y:CGRectGetMidY(self.frame) + (self.frame.height * CGFloat(roads.count-1)));
+            road.addChild(roads[roads.count-1])
+        }
     }
 }
